60822708fe469e5906eda697314dae0122bd57d2
maya-or-psyco <maya.pleau@gmail.com>
2017-07-20 15:32:16 -0400

    Add files via upload

---
+++ quiz/Qscript.js
@@ -0,0 +1,15 @@
+
+function results1(){
+
+
+if (document.getElementById('drama').checked == true){
+  document.getElementById("result").innerHTML = "you chose drama"
+}else if(document.getElementById('suspense').checked == true) {
+document.getElementById("result").innerHTML = "you chose suspense"
+}else if(document.getElementById('horror').checked == true){
+document.getElementById("result").innerHTML = "you chose horror"
+}
+}
+// fuction results()
+//
+// if (value<=50) text = "MARVEL";
